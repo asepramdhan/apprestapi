774fbe85571a474c65b86ef1a03f3aff1dfd8de6
Your Name <asiepramdhan@gmail.com>
2021-05-18 13:14:40 +0700

    berhasil menampilkan data group mahasiswa

--- controller.js
+++ controller.js
@@ -89,3 +89,17 @@ exports.hapusMahasiswa = (req, res) => {
     }
   );
 };
+
+// menampilkan matakuliah group
+exports.tampilGroupMatakuliah = (req, res) => {
+  connection.query(
+    "SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa",
+    (error, rows, fields) => {
+      if (error) {
+        console.log(error);
+      } else {
+        response.oknested(rows, res);
+      }
+    }
+  );
+};
--- res.js
+++ res.js
@@ -8,3 +8,31 @@ exports.ok = function (values, res) {
   res.json(data);
   res.end();
 };
+
+// response untuk nested matakuliah
+exports.oknested = (values, res) => {
+  // lakukan akumulasi
+  const hasil = values.reduce((akumulasikan, item) => {
+    // tentukan key groupnya
+    if (akumulasikan[item.nama]) {
+      // buat variable group nama mahasiswa
+      const group = akumulasikan[item.nama];
+      // cek jika isi array adalah matakuliah
+      if (Array.isArray(group.matakuliah)) {
+        // tambhkan valuenya kedalam group matakuliah
+        group.matakuliah.push(item.matakuliah);
+      } else {
+        group.matakuliah = [group.matakuliah, item.matakuliah];
+      }
+    } else {
+      akumulasikan[item.nama] = item;
+    }
+    return akumulasikan;
+  }, {});
+  let data = {
+    status: 200,
+    values: hasil,
+  };
+  res.json(data);
+  res.end();
+};
